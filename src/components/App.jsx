0cea1d7d39e0afd3113d533bbd5df3d30337b3d7
Simon Gomes <busy.s.simon@gmail.com>
2019-05-06 01:21:30 +0600

    initial app setup

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+import Stories from './Stories';
+
+const App = ({ stories }) => (
+  <div className="app">
+    <Stories stories={stories} />
+  </div>
+);
+
+export default App;
